3d6ffeb26c9b68691373e134353fab37e3aa9420
isimeri tori <loge.lederun@gmail.com>
2024-08-14 12:04:13 +0000

    feat: added loop function

--- number_guess.sh
+++ number_guess.sh
@@ -32,3 +32,43 @@ function greeting(){
     echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
   fi
 }
+
+function loop()
+{
+  if [[ ! $GUESS =~ ^[0-9]+$ ]]
+  then
+    echo "That is not an integer, guess again:"
+    read GUESS
+    loop
+  else
+    # if [[ $GUESS -lt 1 || $GUESS -gt 1000 ]]
+    # then
+    #   # guess is out of bounds
+    #   echo "The guess must be between 1 and 1000."
+    #   read GUESS
+    #   loop
+    if [[ $GUESS -lt $SECRET_NUMBER ]]
+    then
+      # try higher
+      echo "It's higher than that, guess again:"
+      read GUESS
+      NUMBER_OF_GUESSES=$((NUMBER_OF_GUESSES+1))
+      loop
+    elif [[ $GUESS -gt $SECRET_NUMBER ]]
+    then
+      # try lower
+      echo "It's lower than that, guess again:"
+      read GUESS
+      NUMBER_OF_GUESSES=$((NUMBER_OF_GUESSES+1))
+      loop
+    else
+      # win, you guessed the number
+      # increment games count and guesses count
+      NUMBER_OF_GUESSES=$((NUMBER_OF_GUESSES+1))
+      # update db
+      INSERT_NEW_GAME=$($PSQL "insert into games(user_id,number_of_guesses) values($USER_ID, $NUMBER_OF_GUESSES)")
+      # print to console
+      echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+    fi
+  fi
+}
